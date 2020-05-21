b5d9d3d57213da864331d638674addcdae1218a8
mutasov <mutasov@chu.do>
2020-05-22 01:00:10 +0300

    split sources to gradle modules

---
+++ data/build.gradle.kts
@@ -0,0 +1,13 @@
+plugins {
+    kotlin("jvm")
+}
+
+dependencies {
+    implementation(kotlin("stdlib"))
+    testImplementation(kotlin("test"))
+    testImplementation(kotlin("test-junit"))
+}
+
+repositories {
+    jcenter()
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,2 @@
+rootProject.name = "localizator"
+include("app", "data")
